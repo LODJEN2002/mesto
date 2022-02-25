bfe598ea763e4e4a0c804ddf67dfd8e43c6a1709
Ivan Docenko <Decel1337@gmail.com>
2022-02-25 15:28:03 +0500

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,46 +1,31 @@
 let editButton = document.querySelector('.profile__edit-button'); // Кнопка изменения
 let closePopup = document.querySelector('.popup__close-icon') // Кнопка закрытия поп-ап
+let formElement = document.querySelector('.profile__info') // На изменение профиля
+let saveButton = document.querySelector(".popup__container-save") // Кнопка сохранить
+let nameInput = document.querySelector('.popup__container-field-title');
+let jobInput = document.querySelector('.popup__container-field-subtitle');
+let openPopup = document.querySelector('.popup');
+let nameTitle = document.querySelector('.profile__info-text-title');
+let jobSubtitle = document.querySelector('.profile__info-text-subtitle');
 
 function showClick() {
-  let openPopup = document.querySelector('.popup');
   openPopup.classList.add('popup_opened');
 }
 
 editButton.addEventListener('click' , showClick);
 
 function delite() {
-  let openPopup = document.querySelector('.popup');
   openPopup.classList.remove('popup_opened');
 }
 
 closePopup.addEventListener('click' , delite);
 
-
-let formElement = document.querySelector('.profile__info') // На изменение профиля
-
-let saveButton = document.querySelector(".popup__container-save") // Кнопка сохранить
-let nameInput = document.querySelector('.popup__container-field-1');
-let jobInput = document.querySelector('.popup__container-field-2');
-
-
 function formSubmitHandler(evt) {
   evt.preventDefault();
-  let nameTitle = document.querySelector('.profile__info-text-title');
-  let jobSubtitle = document.querySelector('.profile__info-text-subtitle');
-
   nameTitle.textContent = nameInput.value;
   jobSubtitle.textContent = jobInput.value;
-
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
 saveButton.addEventListener('click' , formSubmitHandler);
 saveButton.addEventListener('click' , delite);
-// Это я пытался сделать лайки, к сожалению вышло только так...
-let like = document.querySelector('.elements__group')
-
-function giveLike() {
-  like.setAttribute('src' , 'images/union.svg')
-}
-
-like.addEventListener('click' , giveLike);
