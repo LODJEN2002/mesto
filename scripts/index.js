266fc2bb4832d0b167af374817318aaf3aa33ba2
Ivan Docenko <Decel1337@gmail.com>
2022-02-26 17:20:44 +0500

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let editButton = document.querySelector('.profile__edit-button'); // Кнопка изменения
 let closePopup = document.querySelector('.popup__close-icon') // Кнопка закрытия поп-ап
-let formElement = document.querySelector('.profile__info') // На изменение профиля
+let formElement = document.querySelector('.popup__form') // На изменение профиля
 let saveButton = document.querySelector(".popup__container-save") // Кнопка сохранить
 let nameInput = document.querySelector('.popup__container-field-title');
 let jobInput = document.querySelector('.popup__container-field-subtitle');
@@ -10,12 +10,15 @@ let jobSubtitle = document.querySelector('.profile__info-text-subtitle');
 
 function showClick() {
   openPopup.classList.add('popup_opened');
+
 }
 
 editButton.addEventListener('click' , showClick);
 
 function delite() {
   openPopup.classList.remove('popup_opened');
+  nameInput.value = nameTitle.textContent;
+  jobInput.value = jobSubtitle.textContent;
 }
 
 closePopup.addEventListener('click' , delite);
@@ -24,6 +27,7 @@ function formSubmitHandler (evt) {
   evt.preventDefault();
   nameTitle.textContent = nameInput.value;
   jobSubtitle.textContent = jobInput.value;
+  delite()
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
