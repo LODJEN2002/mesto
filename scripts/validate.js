4aa3c0fb58ebadac7719186b6c5cd6453df7db81
Ivan Docenko <Decel1337@gmail.com>
2022-05-21 13:06:03 +0500

    mesto

--- scripts/validate.js
+++ scripts/validate.js
@@ -1,4 +1,4 @@
-const object = {
+const obj = {
   //форма и инпут
   formSelector: '.popup__form',
   inputSelector: '.popup__input',
@@ -10,80 +10,68 @@ const object = {
   errorClass: 'popup__error_visible'
 }
 
-// Здесь я нахожу через цифры т.к через имя не получается, возможно проблема в дефисах,
-// Готов переделать, но не совсем понимаю как это можно сделать...
-const formProfile = document.forms[0]
-const inputProfileName = formProfile.elements[0]
-const inputProfileJob = formProfile.elements[1]
-const profileButtonSave = formProfile.elements[2]
-
-
-formProfile.addEventListener('input', function (evt) {
-  buttonValid = inputProfileName.value.length > 1 && inputProfileJob.value.length > 1
-  setSubmitButtonState(buttonValid, profileButtonSave ,'popup__container-save-disabled')
-})
-
-function setSubmitButtonState(isFormValid, button, disabledClass) {
-  if(isFormValid) {
-    button.removeAttribute('disabled')
-    button.classList.remove(disabledClass)
-  }else {
-    button.setAttribute('disabled', true)
-    button.classList.add(disabledClass)
-  }
-}
-
-const formCards = document.forms.cards
-const cardsButtonSave = formCards.elements[2]
-
-formCards.addEventListener('input', function (evt) {
-  buttonValid = inputName.value.length > 1 && inputLink.value.startsWith('https://')
-  setSubmitButtonState(buttonValid, cardsButtonSave,'popup-cards__container-save-plus-disabled')
-})
-
-
 //Валидация
-const showInputError = (formElement, inputElement, object) => {
+const showInputError = (formElement, inputElement, obj) => {
   const formError = formElement.querySelector(`.${inputElement.id}-error`);
-  inputElement.classList.add(object.inputErrorClass);
+  inputElement.classList.add(obj.inputErrorClass);
   formError.classList.add('popup__input_form_error');
 }
 
-const hideInputError = (formElement, inputElement, object) => {
+const hideInputError = (formElement, inputElement, obj) => {
   const formError = formElement.querySelector(`.${inputElement.id}-error`)
-  inputElement.classList.remove(object.inputErrorClass)
+  inputElement.classList.remove(obj.inputErrorClass)
   formError.classList.remove('popup__input_form_error');
 }
 
-function isValid(formElement, inputElement, object) {
+function isValid(formElement, inputElement, obj) {
   if(!inputElement.validity.valid) {
-    showInputError(formElement, inputElement, object);
+    showInputError(formElement, inputElement, obj);
   }else {
-    hideInputError(formElement, inputElement, object)
+    hideInputError(formElement, inputElement, obj)
   }
 }
 
 
-const setEventListeners = (formElement, object) => {
-  const inputList = Array.from(formElement.querySelectorAll(object.inputSelector))
+const setEventListeners = (formElement, obj) => {
+  const inputList = Array.from(formElement.querySelectorAll(obj.inputSelector))
+
+  const buttonElement = formElement.querySelector(obj.submitButtonSelector)
 
   inputList.forEach((inputElement) => {
     inputElement.addEventListener('input', () => {
-      isValid(formElement, inputElement, object)
+      isValid(formElement, inputElement, obj);
+
+      toggleButtonState(inputList, buttonElement, obj)
     })
   })
 };
 
 
-const enableValidation = (object) => {
-  const formList = Array.from(document.querySelectorAll(object.formSelector))
+const enableValidation = (obj) => {
+  const formList = Array.from(document.querySelectorAll(obj.formSelector))
 
   formList.forEach((formElement) => {
     formElement.addEventListener('submit', (evt) => {
       evt.preventDefault();
     });
-    setEventListeners(formElement, object);
+    setEventListeners(formElement, obj);
   });
 };
 
-enableValidation(object);
+const hasInvalidInput = (inputList) => {
+  return inputList.some((inputElement) => {
+    return !inputElement.validity.valid
+  })
+}
+
+const toggleButtonState = (inputList, buttonElement, obj) => {
+  if(hasInvalidInput(inputList)) {
+    buttonElement.setAttribute('disabled', true)
+    buttonElement.classList.add(obj.inactiveButtonClass)
+  }else {
+    buttonElement.removeAttribute('disabled')
+    buttonElement.classList.remove(obj.inactiveButtonClass)
+  }
+}
+
+enableValidation(obj);
